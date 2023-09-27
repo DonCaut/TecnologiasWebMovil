5189906a97d41336f980f3bc0ed395bc37ae4b6a
DonCaut <elpolodogmail.com>
2023-09-27 17:20:01 -0300

    Ejercicio2

---
+++ Guía1/Ejercicio2.js
@@ -0,0 +1,5 @@
+let frase=prompt('Ingrese una oración');
+let contador=frase.split(' ');
+let numero=contador.length;
+
+alert('su frase es: \"'+ frase +'\" y el número de palabras es de: '+numero);
